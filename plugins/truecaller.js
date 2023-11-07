90e5eac800c5239ef1d89b6dda9f2f345598a92e
inrl-official <fasweehmon2@gmail.com>
2023-11-06 21:34:30 -0800

    Update truecaller.js

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -13,8 +13,8 @@ inrl({
 		match = match.replace(/login/gi, '');
 		if (!match) return await message.send('_give me a number to send otp_');
 		const msg = await truecaller.set(match);
-		if (msg) return await message.send(`_successfully send otp to this ${match} number_\n_use *true otp* <key> to login_`);
-		return await message.send(`_use *true logout* as first_`)
+		if (msg === true) return await message.send(`_successfully send otp to this ${match} number_\n_use *true otp* <key> to login_`);
+		return await message.send(`*message:* _use *true logout* as first_\n*resone*: ${msg}`);
 	} else if (match.match(/logout/gi)) {
 		await truecaller.logout(match);
 		return await message.send(`_successfull_`);
@@ -22,8 +22,8 @@ inrl({
 		match = match.replace(/otp/gi, '');
 		if (!match) return await message.send('_please provide an otp_');
 		const msg = await truecaller.otp(match);
-		if (msg) return await message.send(`_successfully Logined to Truecaller!!_`);
-		return await message.send(`_use *true logout* as first_`)
+		if (msg === true) return await message.send(`_successfully Logined to Truecaller!!_`);
+		return await message.send(`*message:* _use *true logout* as first_\n*resone*: ${msg}`);
 	}
 	let user = (message.client.mention.jid?.[0] || message.reply_message.mention.jid?.[0] || message.reply_message.text || match).replace(/[^0-9]/g, '');
 	if (!user) return await message.send(`_reply to a user_`)
@@ -35,5 +35,5 @@ inrl({
 		msg += `│ ${key.toLowerCase()}: ${res[key]}\n`;
 	}
 	msg += `╰─❏`;
-	return await message.send('```' + msg + '```',{quoted: message.data})
+	return await message.send('```' + msg + '```', {quoted: message.data})
 });
